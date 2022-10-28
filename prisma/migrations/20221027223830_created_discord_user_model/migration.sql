17012a93f21e4f9959dc731b69e67bdbd7f7111e
Renan Andrade <Fel9.renan02@gmail.com>
2022-10-27 21:18:47 -0300

    Added DiscordUser model

---
+++ prisma/migrations/20221027223830_created_discord_user_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "DiscordUser" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "image" TEXT NOT NULL
+);
